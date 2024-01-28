14ff6807f5194a39a464513a86c53a2ef8af44a0
bipul8282 <119596693+bipul8282@users.noreply.github.com>
2024-01-28 23:16:42 +0530

    almost

--- src/Product.js
+++ src/Product.js
@@ -1,28 +1,35 @@
 import React from 'react';
-import './Product.css'; // Make sure to create a CSS file for styling
+import './Product.css'; 
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 
 function Product() {
   return (
+    <>
+    <h1>Products</h1>
     <div className='Pro'>
-        <h1>Products</h1>
+        
       <div className='ProBox'>
         <div className='Pro1'>
           <h1>22W USB charger socket</h1>
           <h4>Increase consumer productivity and reduce charge anxiety</h4>
-          <ShoppingCartOutlinedIcon/>
+          <div className='Cart'>
+          <ShoppingCartOutlinedIcon />
+          </div>
           <img src='https://img2.exportersindia.com/product_images/bc-full/2023/10/12687155/mavolo-rus-1698399745_7147207_1940144.jpg'/>
           
         </div>
         <div className='Pro2'>
           <h1>12W USB charger socket</h1>
           <h4>Increase consumer productivity and reduce charge anxiety</h4>
-          <ShoppingCartOutlinedIcon/>
+          <div className='Cart'>
+          <ShoppingCartOutlinedIcon />
+          </div>
           <img src='https://5.imimg.com/data5/SELLER/Default/2023/10/352693383/TP/BX/PT/85281307/mavolo-single-port-usb-charger-socket-500x500.jpg'/>
           
         </div>
       </div>
     </div>
+    </>
   );
 }
 
